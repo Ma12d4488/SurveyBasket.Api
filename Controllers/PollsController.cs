d448108829db4e827a54308d07a022e1f505a7a2
mahmoud122003 <92595486+Ma12d4488@users.noreply.github.com>
2025-03-21 22:12:08 +0200

    Add CRUD Opration to Polls Controllar

---
+++ Controllers/PollsController.cs
@@ -0,0 +1,49 @@
+﻿namespace SurveyBasket.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class PollsController(IPollServices pollServices) : ControllerBase
+{
+    private readonly IPollServices _pollServices = pollServices;
+
+    [HttpGet("getall")]
+    public IActionResult GetAll() 
+    {
+        return Ok(_pollServices.GetAll());
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult Get (int id)
+    {
+        var poll = _pollServices.Get(id);
+
+        return poll is null ? NotFound() : Ok(poll);
+    }
+
+    [HttpPost("")]
+    public IActionResult Add(Poll request)
+    {
+        var newPoll = _pollServices.Add(request);
+
+        return CreatedAtAction(nameof(Get), new { id = newPoll.Id }, newPoll);
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult Update(int id, Poll request)
+    {
+        if (!_pollServices.Update(id, request))
+            return NotFound();
+
+        return NoContent();
+    }
+
+
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+        if (!_pollServices.Delete(id))
+            return NotFound();
+
+        return Ok();
+    }
+}
---
+++ Services/IPollServices.cs
@@ -0,0 +1,13 @@
+﻿namespace SurveyBasket.Api.Services;
+
+public interface IPollServices
+{
+    IEnumerable<Poll> GetAll();
+
+    Poll? Get(int id);
+
+    Poll Add(Poll poll);
+
+    bool Update(int id, Poll poll);
+    bool Delete(int id);
+}
